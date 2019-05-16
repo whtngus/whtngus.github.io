0aefe60729d0fe8e03cfd63d1c72080b8d01a59f
Suhyun <whtngus3232@naver.com>
2019-05-17 01:31:12 +0900

    - 데이터 임베딩 작업중

---
+++ kaggle/outbrain_click-prediction/README.md
@@ -0,0 +1,42 @@
+# 데이터 설명
+
+### page_views.csv
+    ID : 사용자 식별자
+    ad_id : 광고 식별자
+    advertiser_id : 광고주 식별자
+    campaign_id : 캠페인
+
+### page_views.csv
+    uuid
+    document_id
+    timestamp  : 1465876799998을 빼면 원래 시간
+    platform  : desktop = 1, mobile = 2, tablet =3
+    geo_location  : country>state>DMA
+    traffic_source : internal = 1, search = 2, social = 3
+
+### clicks_train.csv
+        display_id
+        ad_id
+        clicked  :  1 클릭함, 0 클릭 안함
+        
+### events.csv
+           display_id
+           uuid
+           document_id
+           timestamp
+           platform
+           geo_location
+         
+### promoted_content.csv
+        ad_id
+        document_id
+        campaign_id
+        advertiser_id
+        
+###documents_meta.csv
+> document 상세 정보 제공 <br>
+
+        document_id
+        source_id  : 사이트 url 정보인듯
+        publisher_id
+        publish_time
---
+++ kaggle/outbrain_click-prediction/train/data/DataLoader.py
@@ -0,0 +1,46 @@
+# -*- coding: utf_8 -*-
+import sys
+import csv
+import numpy as np
+import pandas as pd
+import os
+
+class DataLoader:
+    def __init__(self,events_path,promoted_content_path):
+        self.promoted_content, self.events = self.default_data_set(promoted_content_path,events_path)
+
+    def default_data_set(self,promoted_content_path,events_path):
+        promoted_content = pd.read_csv(promoted_content_path)
+        events  = pd.read_csv(events_path)
+        return promoted_content, events
+
+    def train_loader(self,clicks_train_path):
+        data_list = []
+        label_list = []
+
+        # 모든 데이터 붙이기
+        train = pd.read_csv(clicks_train_path,nrows=1000)
+        train = pd.merge(train,self.promoted_content,on="ad_id")
+        train = pd.merge(train,self.events,on="display_id")
+        label_list = train["clicked"].values
+        # 'display_id', 'ad_id', 'clicked', 'document_id_x', 'campaign_id',
+        #        'advertiser_id', 'uuid', 'document_id_y', 'timestamp', 'platform',
+        #        'geo_location'
+        # 뽑아낼 수 있는 데이터 : timestamp, platform , geo_location
+        train = train[["geo_location","platform","geo_location"]]
+
+        print()
+
+
+        data_list = np.array(data_list)
+        return data_list, label_list
+
+
+
+if __name__ == "__main__":
+    events_path = "../data/events.csv"
+    promoted_content_path = "../data/promoted_content.csv"
+    # extract_data = ["Survived","Pclass","Sex","Age","SibSp","Parch","Ticket","Fare","Cabin","Embarked","PassengerId"]
+    dataLoader = DataLoader(events_path,promoted_content_path)
+    clicks_train_path = "../data/clicks_train.csv"
+    train_input, train_label = dataLoader.train_loader(clicks_train_path)
